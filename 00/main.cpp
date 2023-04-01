63c9fe9914d56326500dde9f134d217679067545
scc <chongchenshi@gmail.com>
2023-04-01 22:51:39 +0800

    第一次提交

---
+++ 00/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "scc" << std::endl;
+}
---
+++ 02/test.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << add(1, 2) << std::endl;
+    return 0;
+}
